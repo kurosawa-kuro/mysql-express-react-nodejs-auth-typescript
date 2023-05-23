858e37251b7109d89f722d5d88b01b49b6cfd3c2
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-24 04:03:50 +0900

    fix ref order

--- backend/index.ts
+++ backend/index.ts
@@ -1,46 +1,43 @@
-// backend\index.ts
-
 import path from 'path';
 import express, { Express, Request, Response } from 'express';
-import cors, { CorsOptions } from "cors";
+import cors from 'cors';
 import dotenv from 'dotenv';
-dotenv.config();
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware';
 import { router as userRoutes } from './routes/userRoutes';
 
+dotenv.config();
+
 export const app: Express = express();
 
-const corsOptions: CorsOptions = {
-    origin: "http://localhost:3000",
+// Middleware
+app.use(cors({
+    origin: 'http://localhost:3000',
     credentials: true,
-};
-app.use(cors(corsOptions));
-
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
+// API Routes
 app.use('/api/users', userRoutes);
 
+// Static files and default route
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-    app.get('*', (req: Request, res: Response) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-    );
+    app.get('*', (req: Request, res: Response) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
 } else {
     app.get('/', (req: Request, res: Response) => {
         res.send('API is running....');
     });
 }
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('API is running....');
-});
-
+// Error handling
 app.use(notFound);
 app.use(errorHandler);
 
+export default app;
