a4428a778b2353c49e4b5e7e40134d40c2b6fbff
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-25 14:31:13 +0900

    WIP Ref order

--- frontend/src/__tests__/UserLoginScreen.test.tsx
+++ frontend/src/__tests__/UserLoginScreen.test.tsx
@@ -2,17 +2,34 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import LoginScreen from '../screens/auth/LoginScreen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { useNavigate } from 'react-router-dom';
 
 const mockSetEmail = jest.fn();
 const mockSetPassword = jest.fn();
 const mockSubmitHandler = jest.fn();
-
-// Create a standalone mock function
-let mockUseLoginUserHook = jest.fn();
 const mockNavigate = jest.fn();
 
-// Mock the loginUserApi function
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate,
+}));
+
+jest.mock('@tanstack/react-query', () => {
+    const actualReactQuery = jest.requireActual('@tanstack/react-query');
+    return {
+        ...actualReactQuery,
+        useMutation: jest.fn(() => ({
+            mutate: jest.fn((_data, options) => {
+                options.onSuccess();
+            }),
+            isLoading: false,
+        })),
+        QueryClient: jest.fn(() => ({
+            clear: jest.fn(),
+        })),
+        QueryClientProvider: jest.fn(({ children }) => <>{children}</>),
+    };
+});
+
 jest.mock('../services/api', () => ({
     ...jest.requireActual('../services/api'),
     loginUserApi: jest.fn().mockResolvedValue({
@@ -24,53 +41,16 @@ jest.mock('../services/api', () => ({
     }),
 }));
 
-// Mock the useMutation hook
-jest.mock('@tanstack/react-query', () => ({
-    ...jest.requireActual('@tanstack/react-query'),
-    useMutation: jest.fn(() => ({
-        mutate: jest.fn((_data, options) => {
-            options.onSuccess();
-        }),
-        isLoading: false,
-    })),
-}));
-
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: () => mockNavigate,
-}));
-
-
-jest.mock('../hooks/auth/useLoginUserHook', () => {
-    return {
-        useLoginUserHook: () => ({
-            mutation: { isLoading: false, isSuccess: true },
-            submitHandler: mockSubmitHandler,
-            email: '',
-            setEmail: mockSetEmail,
-            password: '',
-            setPassword: mockSetPassword,
-            navigate: mockNavigate, // Add this line
-        }),
-    };
-});
-
-
-
-jest.mock('@tanstack/react-query', () => {
-    const actualReactQuery = jest.requireActual('@tanstack/react-query');
-    return {
-        ...actualReactQuery,
-        useMutation: () => ({
-            mutate: jest.fn(),
-            isLoading: false,
-        }),
-    };
-});
-
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: jest.fn(),
+jest.mock('../hooks/auth/useLoginUserHook', () => ({
+    useLoginUserHook: () => ({
+        mutation: { isLoading: false, isSuccess: true },
+        handleFormSubmit: mockSubmitHandler,
+        email: '',
+        setEmail: mockSetEmail,
+        password: '',
+        setPassword: mockSetPassword,
+        navigate: mockNavigate,
+    }),
 }));
 
 const queryClient = new QueryClient();
@@ -78,15 +58,6 @@ const queryClient = new QueryClient();
 beforeEach(() => {
     jest.clearAllMocks();
     window.HTMLFormElement.prototype.requestSubmit = () => { };
-    mockUseLoginUserHook.mockReturnValue({
-        mutation: { isLoading: false, isSuccess: true },
-        submitHandler: mockSubmitHandler,
-        email: '',
-        setEmail: mockSetEmail,
-        password: '',
-        setPassword: mockSetPassword,
-        navigate: mockNavigate,
-    });
 });
 
 test('renders LoginScreen with Sign In h1 header and input fields', () => {
@@ -127,7 +98,7 @@ test('updates email and password values on input change', () => {
     expect(mockSetPassword).toHaveBeenCalledWith('password123');
 });
 
-test('calls submitHandler on form submission', () => {
+test('calls handleFormSubmit on form submission', () => {
     render(
         <QueryClientProvider client={queryClient}>
             <Router>
@@ -142,31 +113,7 @@ test('calls submitHandler on form submission', () => {
     expect(mockSubmitHandler).toHaveBeenCalled();
 });
 
-// test('renders Loader when API call is loading', () => {
-//     mockUseLoginUserHook.mockReturnValue({
-//         mutation: { isLoading: true },
-//         submitHandler: mockSubmitHandler,
-//         email: '',
-//         setEmail: mockSetEmail,
-//         password: '',
-//         setPassword: mockSetPassword,
-//     });
-
-//     render(
-//         <QueryClientProvider client={queryClient}>
-//             <Router>
-//                 <LoginScreen />
-//             </Router>
-//         </QueryClientProvider
-//     );
-
-//     expect(screen.getByTestId('loader')).toBeInTheDocument();
-// });
-
-test('redirects to home screen after successful login', async () => {
-    const { useLoginUserHook } = require('../hooks/auth/useLoginUserHook');
-    const { navigate } = useLoginUserHook();
-
+test('renders Loader when API call is loading', () => {
     render(
         <QueryClientProvider client={queryClient}>
             <Router>
@@ -175,19 +122,32 @@ test('redirects to home screen after successful login', async () => {
         </QueryClientProvider>
     );
 
-    // Fill out the form
-    fireEvent.change(screen.getByPlaceholderText('Enter email'), {
+    expect(screen.getByTestId('loader')).toBeInTheDocument();
+});
+
+test('redirects to home screen after successful login', async () => {
+    render(
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <LoginScreen />
+            </Router>
+        </QueryClientProvider>
+    );
+
+    const emailInput = screen.getByPlaceholderText('Enter email');
+    const passwordInput = screen.getByPlaceholderText('Enter password');
+
+    fireEvent.change(emailInput, {
         target: { value: 'test@example.com' },
     });
-    fireEvent.change(screen.getByPlaceholderText('Enter password'), {
+    fireEvent.change(passwordInput, {
         target: { value: 'password123' },
     });
 
-    // Submit the form
     fireEvent.submit(screen.getByTestId('login-form'));
 
     await waitFor(() => {
-        expect(navigate).toHaveBeenCalledWith('/');
+        expect(mockSubmitHandler).toHaveBeenCalled();
+        expect(mockNavigate).toHaveBeenCalledWith('/');
     });
 });
-
