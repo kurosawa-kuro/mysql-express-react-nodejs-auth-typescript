3f506bca418aceba3d33676353f6729fa16b8e8e
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-28 14:00:54 +0900

    big sucess ログインフォームにEmailとPWを入力して送信した時、非同期通信処理が呼び出されるか

--- frontend/src/__tests__/UserLoginScreen.test.tsx
+++ frontend/src/__tests__/UserLoginScreen.test.tsx
@@ -1,37 +1,60 @@
-// Testing packages
+// テストに必要なパッケージをインポート
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
-// Component to test
+// テスト対象のコンポーネントをインポート
 import LoginScreen from '../screens/auth/LoginScreen';
 
-// Mocking the loginUserApi
+// loginUserApiのモックを作成
 import { loginUserApi } from '../services/api';
 jest.mock('../services/api');
 
-test('ログインフォームにEmailとPWを入力してsubmitした時に非同期通信処理が呼ばれるかのテスト', async () => {
+// useNavigateをモック化
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn(),
+}));
+
+test('ログインフォームにEmailとPWを入力して送信した時、非同期通信処理が呼び出されるか', async () => {
+    // loginUserApiのモック関数を作成
     const mockLoginUserApi = loginUserApi as jest.MockedFunction<typeof loginUserApi>;
 
+    // テスト用のメールアドレスとパスワードを設定
     const email = "test@example.com";
     const password = "password";
 
+    // モックユーザーを作成
+    const user = { id: 1, email: 'test@example.com', name: 'Test User', isAdmin: false };
+
+    // loginUserApiがユーザーを返すように設定
+    mockLoginUserApi.mockResolvedValue(user);
+
+    // useNavigateのモックを作成
+    const navigate = jest.fn();
+    (require('react-router-dom').useNavigate as jest.Mock).mockReturnValue(navigate);
+
+    // レンダリング
     const { getByPlaceholderText, getByTestId } = render(
         <MemoryRouter>
             <LoginScreen />
         </MemoryRouter>
     );
 
+    // 入力フィールドと送信ボタンを取得
     const emailInput = getByPlaceholderText('Enter email');
     const passwordInput = getByPlaceholderText('Enter password');
     const submitButton = getByTestId('login-form');
 
-    // Simulating user input and form submission
+    // ユーザーの入力とフォームの送信をシミュレート
     fireEvent.change(emailInput, { target: { value: email } });
     fireEvent.change(passwordInput, { target: { value: password } });
     fireEvent.submit(submitButton);
 
-    // Expect loginUserApi to have been called
+    // loginUserApiが呼び出されたことを確認
     await waitFor(() => {
         expect(mockLoginUserApi).toHaveBeenCalledWith({ email, password });
     });
+
+    // navigateが正しいパスで呼び出されたことを確認
+    expect(navigate).toHaveBeenCalledWith('/');
 });
--- frontend/src/screens/auth/LoginScreen.tsx
+++ frontend/src/screens/auth/LoginScreen.tsx
@@ -1,3 +1,5 @@
+// frontend\src\screens\auth\LoginScreen.tsx
+
 // React Packages
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
