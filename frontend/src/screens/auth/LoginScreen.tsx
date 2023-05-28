d844bde4a054ebd76a091d9a2f6b7d97ef925bcf
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-28 13:16:07 +0900

    rm custom hook

--- frontend/src/screens/auth/LoginScreen.tsx
+++ frontend/src/screens/auth/LoginScreen.tsx
@@ -3,21 +3,52 @@
 // External Packages
 import { Link } from 'react-router-dom';
 import { Loader } from '../../components/Loader';
-
-// Internal Modules
-import { useLoginUserHook } from '../../hooks/auth/useLoginUserHook';
+import { useState } from 'react';
+import { AxiosError } from 'axios';
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useUserStore, useFlashMessageStore } from '../../state/store';
+import { loginUserApi, ApiError } from '../../services/api';
 
 const LoginScreen = () => {
-    const {
-        mutation: loginUserApiMutation,
-        submitHandler,
-        email,
-        setEmail,
-        password,
-        setPassword
-    } = useLoginUserHook();
+    // Navigation
+    const navigate = useNavigate();
 
-    // Component JSX
+    // Local State
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Global State
+    const { setUser } = useUserStore();
+    const { setFlashMessage } = useFlashMessageStore();
+
+    // API mutation
+    const loginUserApiMutation = useMutation(
+        async () => {
+            const user = await loginUserApi({ email, password });
+            return user;
+        },
+        {
+            // On success
+            onSuccess: (user) => {
+                setUser(user);
+                setFlashMessage("User login successful!");
+                navigate('/');
+            },
+            // On error
+            onError: (error: AxiosError<ApiError>) => {
+                toast.error(error?.response?.data?.message || error.message);
+            },
+        }
+    );
+
+    // Form submit handler
+    const submitHandler = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        loginUserApiMutation.mutate();
+    };
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <h1 className="text-3xl font-bold mb-4">Log in</h1>
